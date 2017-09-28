a6e7f4292547acf6f817429aeefd3134c7de066b
herve <herve@herve-PC>
2017-09-28 02:46:43 +0200

    INIT - First Commit

---
+++ french-to-javanais/src/test/java/fr/poleemploi/test/FrenchToJavanaisTest.java
@@ -0,0 +1,38 @@
+package fr.poleemploi.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FrenchToJavanaisTest {
+	private static String moyen = "moyen";
+	private static final String mavoyen = "mavoyen";
+	private static String exemple = "exemple";
+	private static final String avexavemplave = "avexavemplave";
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
